b60319041da7ead45f4bf72b0309866ce2ceb850
Thomas Walther <tho.walther@gmail.com>
2020-07-17 11:41:29 +0200

    Update WebcamCounter.py

--- WebcamCounter.py
+++ WebcamCounter.py
@@ -65,11 +65,11 @@ if __name__ == '__main__':
 
     client_s3 = boto3.client("s3" )
 
-    response = client_s3.put_object(
-        Bucket="sdd-s3-bucket",
-        Body=json.dumps(webcams),
-        Key=f"webcamdaten/{datetime.now().strftime('%Y/%m/%d/%H')}webcamdaten-customvision.json"
-      )
+    #response = client_s3.put_object(
+    #    Bucket="sdd-s3-bucket",
+    #    Body=json.dumps(webcams),
+    #   Key=f"webcamdaten/{datetime.now().strftime('%Y/%m/%d/%H')}webcamdaten-customvision.json"
+    # )
     
     #directory = r'/tmp'
     #for filename in os.listdir(directory):
