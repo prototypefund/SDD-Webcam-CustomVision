612eaa78bdf51d6f5960c05925854e1ddc6dfe71
Thomas Walther <tho.walther@gmail.com>
2020-07-27 19:00:47 +0200

    Update WebcamCounter.py

--- WebcamCounter.py
+++ WebcamCounter.py
@@ -28,11 +28,8 @@ class PeopleCounter:
            headers={
            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
            }
-        )
-        sock=urllib.urlretrieve(url)
-        capture = cv.CaptureFromFile(sock[0])
-        resp = cv.QueryFrame(capture)
-        #resp = urllib.request.urlopen(req, timeout=10)
+        )     
+        resp = urllib.request.urlopen(req, timeout=10)
         #resp = urllib.request.urlopen(url, timeout=10)
         self.image = np.asarray(bytearray(resp.read()), dtype="uint8")
         #if self.img is not None:
